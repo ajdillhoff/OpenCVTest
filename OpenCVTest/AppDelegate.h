34ac80513f50efdcc3fb47952d2c58fbb1b15363
Alex Dillhoff <alex.dillhoff@ayokasystems.com>
2014-06-30 12:30:58 -0500

    Initial Commit

---
+++ OpenCVTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  OpenCVTest
+//
+//  Created by Ayoka Systems on 6/30/14.
+//  Copyright (c) 2014 Ayoka Systems. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
